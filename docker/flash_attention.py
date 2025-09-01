b1ca61f38ca9ac118be27e6ac80103f9df79b1c3
deciding <zhangzn710@gmail.com>
2025-09-01 15:14:01 +0000

    finished added bench for modal

--- docker/flash_attention.py
+++ docker/flash_attention.py
@@ -435,8 +435,20 @@ def test_flash_attention():
                 if txl.is_warpgroup([2]):
                     desc_o.store([qo_offset_y+BLOCK_M//2, 0], acc.to(dtype))
 
+        def is_hip():
+            return triton.runtime.driver.active.get_current_target().backend == "hip"
+
+
+        def is_cuda():
+            return triton.runtime.driver.active.get_current_target().backend == "cuda"
+
+
         def supports_host_descriptor():
-            return True
+            return is_cuda() and torch.cuda.get_device_capability()[0] >= 9
+
+
+        def is_blackwell():
+            return is_cuda() and torch.cuda.get_device_capability()[0] == 10
 
         class _attention(torch.autograd.Function):
 
@@ -540,9 +552,87 @@ def test_flash_attention():
             print(f"Output mean diff: {(tri_out - ref_out).abs().mean().item()}")
             assert torch.allclose(ref_out, tri_out, atol=1e-2, rtol=0)
 
+        TORCH_HAS_FP8 = hasattr(torch, 'float8_e5m2')
+        BATCH, N_HEADS, HEAD_DIM = 16, 32, 128
+
+        TORCH_HAS_FP8=False
+        # vary seq length for fixed head and batch=4
+        configs = []
+        for mode in ["fwd"]:
+            for causal in [False]:
+                for warp_specialize in [False, True] if is_blackwell() else [False]:
+                    if mode == "bwd" and not causal:
+                        continue
+                    configs.append(
+                        triton.testing.Benchmark(
+                            x_names=["N_CTX"],
+                            x_vals=[2**i for i in range(10, 15)],
+                            line_arg="provider",
+                            line_vals=(["triton-fp16"] if Has_TXL else []) + (["triton-fp8"] if TORCH_HAS_FP8 else []) +
+                            (["flash"] if HAS_FLASH else []),
+                            line_names=(["Triton [FP16]"] if Has_TXL else [])  + (["Triton [FP8]"] if TORCH_HAS_FP8 else []) +
+                            (["Flash-3"] if HAS_FLASH else []),
+                            styles=[("red", "-"), ("blue", "-"), ("green", "-")],
+                            ylabel="TFLOPS",
+                            plot_name=
+                            f"fused-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}-{mode}-causal={causal}-warp_specialize={warp_specialize}",
+                            args={
+                                "H": N_HEADS,
+                                "BATCH": BATCH,
+                                "HEAD_DIM": HEAD_DIM,
+                                "mode": mode,
+                                "causal": causal,
+                            },
+                        ))
+        @triton.testing.perf_report(configs)
+        def bench_flash_attention(BATCH, H, N_CTX, HEAD_DIM, causal, mode, provider, device=DEVICE, algo=0, no_tune=False):
+            # follow fa3 paper
+            BATCH = int(16384 / N_CTX)
+            assert mode in ["fwd", "bwd"]
+            dtype = torch.float16
+            q = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device)
+            k = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device)
+            v = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device)
+            if "triton" in provider:
+                if mode == "fwd" and "fp8" in provider:
+                    q = q.to(torch.float8_e5m2)
+                    k = k.to(torch.float8_e5m2)
+                    v = v.permute(0, 1, 3, 2).contiguous()
+                    v = v.permute(0, 1, 3, 2)
+                    v = v.to(torch.float8_e5m2)
+                sm_scale = 1/math.sqrt(HEAD_DIM)
+                fn = lambda: attention(q, k, v, causal, sm_scale, algo, no_tune)
+                if mode == "bwd":
+                    o = fn()
+                    do = torch.randn_like(o)
+                    fn = lambda: o.backward(do, retain_graph=True)
+                ms = triton.testing.do_bench(fn, warmup=1000, rep=1000)
+
+            if provider == "flash":
+                # qkv = torch.randn((BATCH, N_CTX, 3, H, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
+                # fn = lambda: flash_attn_func(qkv, causal=causal)
+                q = q.permute(0,2,1,3).contiguous()
+                k = k.permute(0,2,1,3).contiguous()
+                v = v.permute(0,2,1,3).contiguous()
+                fn = lambda: flash_attn_func(q, k, v, causal=causal)
+                if mode == "bwd":
+                    o = fn()
+                    do = torch.randn_like(o)
+                    fn = lambda: o.backward(do, retain_graph=True)
+                ms = triton.testing.do_bench(fn, warmup=1000, rep=1000)
+
+            flops_per_matmul = 2.0 * BATCH * H * N_CTX * N_CTX * HEAD_DIM
+            total_flops = 2 * flops_per_matmul
+            if causal:
+                total_flops *= 0.5
+            if mode == "bwd":
+                total_flops *= 2.5  # 2.0(bwd) + 0.5(recompute)
+            return total_flops * 1e-12 / (ms * 1e-3)
+
         no_tune=True
         PROFILING=False
         test_op(16, 32, 1024, 128, False, dtype=torch.float16, algo=3, no_tune=no_tune, profiling=PROFILING)
+        bench_flash_attention.run(save_path=".", print_data=True, algo=3, no_tune=no_tune)
 
     test_torch()
     test_txl()
